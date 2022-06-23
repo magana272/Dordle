6c536b39994378c13e1c364cc958beb6f58dc501
manuel <magana272@berkeley.edu>
2022-06-23 19:10:39 -0400

    Updates

---
+++ precache-manifest.f0dcda6bbb2b4568f1b53e464fb9a000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49f4a4b7a6c54faa4999",
+    "url": "/Dordle/static/css/main.8ac743a8.chunk.css"
+  },
+  {
+    "revision": "49f4a4b7a6c54faa4999",
+    "url": "/Dordle/static/js/main.49f4a4b7.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Dordle/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "eddd1ea37c963bedcb97",
+    "url": "/Dordle/static/js/2.eddd1ea3.chunk.js"
+  },
+  {
+    "revision": "da7143fbb6752fe401bc",
+    "url": "/Dordle/static/js/runtime~main.da7143fb.js"
+  },
+  {
+    "revision": "b0f8c0a0be5c7d16062f7c77167d1600",
+    "url": "/Dordle/index.html"
+  }
+];
